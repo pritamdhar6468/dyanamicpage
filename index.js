92e71f10bd0179971da0d802c0d7d2def61e9ee5
Pritamdhar_official <pritamdhar6568@gmail.com>
2022-12-11 10:15:28 +0530

    Project done
    successfully complete the project.

--- index.js
+++ index.js
@@ -4,6 +4,8 @@ greeting = document.getElementById('greetings'),
 name = document.getElementById('name'),
 goal = document.getElementById('goal');
 
+const showAmPm=true;
+
 //show time
 function showTime() {
     let today = new Date(),
@@ -16,7 +18,7 @@ function showTime() {
     //12hr format
     hour = hour % 12 || 12;
     //output
-    time.innerHTML = `${hour}<span>:</span>${addZero(min)}<span>:</span>${addZero(sec)}`;
+    time.innerHTML = `${hour}<span>:</span>${addZero(min)}<span>:</span>${addZero(sec)} ${showAmPm ? ampm:''}`;
 
     setTimeout(showTime,1000);
 }
@@ -58,6 +60,20 @@ function getName(){
     }
 }
 
+//set name
+function setName(e){
+    if(e.type==='keypress'){
+        //make sure enter is pressed
+        if(e.which==13|| e.keyCode==13){
+            localStorage.setItem('name',e.target.innerText);
+            name.blur();
+        }
+    }else{
+        localStorage.setItem('name',e.target.innerText);
+
+    }
+}
+
 //get goal
 function getGoal(){
     if(localStorage.getItem('goal')=== null) {
@@ -66,6 +82,25 @@ function getGoal(){
         goal.textContent=localStorage.getItem('goal');
     }
 }
+
+//set goal
+function setGoal(e){
+    if(e.type==='keypress'){
+        //make sure enter is pressed
+        if(e.which==13|| e.keyCode==13){
+            localStorage.setItem('goal',e.target.innerText);
+            goal.blur();
+        }
+    }else{
+        localStorage.setItem('goal',e.target.innerText);
+
+    }
+}
+
+name.addEventListener('keypress', setName);
+name.addEventListener('blur', setName);
+goal.addEventListener('keypress', setGoal);
+goal.addEventListener('blur', setGoal);
 
 //run
 showTime();
